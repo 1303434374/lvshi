cc343b3aacae39e9b717e0ffd12f7dab2898b648
1303434374 <35438089+1303434374@users.noreply.github.com>
2018-09-27 15:45:23 +0800

    全部上传

---
+++ siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "京佳律师",
+    uniacid: "4",
+    acid: "4",
+    multiid: "0",
+    version: "1.6.2",
+  siteroot: "https://lvshi.webyiy.com/app/index.php",
+    design_method: "4"
+};
